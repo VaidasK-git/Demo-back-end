a85f05904c47257a56e8fe156201b634101d95cc
VaidasK-git <vaidaskc@gmail.com>
2022-11-04 09:08:29 +0200

    Initial commit

---
+++ src/main/java/lt/kucinskas/demobackend/DemoBackEndApplication.java
@@ -0,0 +1,13 @@
+package lt.kucinskas.demobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/kucinskas/demobackend/DemoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.kucinskas.demobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
